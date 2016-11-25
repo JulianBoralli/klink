f0a794d8cbc5d056156c6f7335ae1cfb81bdc9b7
Sierra McLawhorn <sierramclawhorn@gmail.com>
2016-11-25 14:21:06 -0800

    Add logic for repsonsive canvases

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,10 +1,81 @@
 $( document ).on('turbolinks:load', function() {
-	
-	var canvasPlay = new fabric.Canvas('canvas-play');
+   
+  var ResponsiveCanvas = fabric.util.createClass(fabric.Canvas, {
+    getPointer: function () {
+        this.calcOffset();
+        return this.callSuper('getPointer');
+    },
+    setWidth: function (value, options) {
+      return this.setDimensions({ width: value }, options);
+    },
 
-	var canvasPalette = new fabric.Canvas('canvas-palette');
+    setHeight: function (value, options) {
+      return this.setDimensions({ height: value }, options);
+    },
+
+    setDimensions: function (dimensions, options) {
+      var cssValue;
+
+      options = options || {};
+
+      for (var prop in dimensions) {
+        cssValue = dimensions[prop];
+
+        !options.cssOnly ? (this._setBackstoreDimension(prop, dimensions[prop]), cssValue += 'px') : '';
+        !options.backstoreOnly ? this._setCssDimension(prop, cssValue) : '';
+      }
+
+      !options.cssOnly ? this.renderAll() : '';
+      this.calcOffset();
+
+      return this;
+    },
+
+    _setBackstoreDimension: function (prop, value) {
+      this.lowerCanvasEl[prop] = value;
+
+      if (this.upperCanvasEl) {
+        this.upperCanvasEl[prop] = value;
+      }
+
+      if (this.cacheCanvasEl) {
+        this.cacheCanvasEl[prop] = value;
+      }
+
+      this[prop] = value;
+
+      return this;
+    },
+
+    _setCssDimension: function (prop, value) {
+      this.lowerCanvasEl.style[prop] = value;
+
+      if (this.upperCanvasEl) {
+        this.upperCanvasEl.style[prop] = value;
+      }
+
+      if (this.wrapperEl) {
+        this.wrapperEl.style[prop] = value;
+      }
+
+      return this;
+    }
+  });
+
+  
+  var canvasPlay = new ResponsiveCanvas('canvas-play');
+
+	var canvasPalette = new ResponsiveCanvas('canvas-palette');
 
 	generatePalette(canvasPlay, canvasPalette);
+
+  canvasPlay.setDimensions({width: '90vw', height: '60vh'}, {
+    cssOnly: true
+  });
+
+  canvasPalette.setDimensions({width: '90vw', height: '20vh'}, {
+    cssOnly: true
+  });
 
 	canvasPalette.selectable = true;
 
