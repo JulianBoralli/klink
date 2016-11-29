0f1e919cc3b6824ce78248cebbfaf2a4fd2411fc
Robbie Santos <robbiealixsantos@gmail.com>
2016-11-29 13:47:47 -0800

    add preliminary rspec tests for game features

--- spec/features/drawings_spec.rb
+++ spec/features/drawings_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.feature "Drawings", type: :feature do
+include RSpecHtmlMatchers
   context 'a user' do
     describe 'initial page load' do
-      include RSpecHtmlMatchers
 
       it 'gives a selection for backgrounds' do
         visit draw_path
@@ -13,6 +13,13 @@ RSpec.feature "Drawings", type: :feature do
       it 'loads with images to select from' do
         visit draw_path
         expect('<img src="images/landscape.png" class="drawing-bg" id="land-bg">').to have_tag('img')
+      end
+    end
+
+    describe 'after background selection' do
+      it 'brings up the selection tools for drawing' do
+        visit draw_path
+        expect('<div id="drawing-mode-options">').to have_tag('div',  :with => { :id => 'drawing-mode-options' })
       end
     end
   end
