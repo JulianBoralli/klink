81be8bf304ae46f5b3f3077a8bfdba6d585bf183
Katherine Bui <kaatherine.buii@gmail.com>
2016-11-28 19:55:12 -0800

    Add logo animation and new memory photo

--- app/assets/javascripts/splashPage.js
+++ app/assets/javascripts/splashPage.js
@@ -12,4 +12,13 @@ $( document ).on('turbolinks:load', function() {
     $('#game-logo').fadeIn();
   }, 5000)
 
+  $('#banner').on('click', function(){
+    $(this).addClass('animated rubberBand')
+      setTimeout(function(){
+        $('#banner').removeClass('animated rubberBand')
+    }, 300)
+  });
+
+
+
 });
