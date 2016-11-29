44836d0cd9f78da5e21509165edf6364396ba5f5
Katherine Bui <kaatherine.buii@gmail.com>
2016-11-28 19:35:53 -0800

    All icons appear in the right order, IT WORKS

--- app/assets/javascripts/channels/splashPage.js
+++ app/assets/javascripts/channels/splashPage.js
@@ -1,13 +1,15 @@
 $( document ).on('turbolinks:load', function() {
 
+  $('#game-logo').hide();
   setTimeout(function(){
-   document.getElementById('fade').className = 'disappear';
-   $('#fade').hide();
+    document.getElementById('fade').className = 'disappear';
+    $('#fade').hide();
+    var fade = $('#banner').fadeIn('slow')
+    fade.addClass('small').animate({'margin-top':'0'})
+  }, 2200);
 
-   var fade = $('#banner').fadeIn('slow');
-   fade.addClass('small').animate({'margin-top':'-30'});
-
-   
- }, 2800);
+  setTimeout(function(){
+    $('#game-logo').fadeIn();
+  }, 5000)
 
 });
