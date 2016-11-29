3284524cb6e3466892397b3345a55bd74a38dc8a
Robbie Santos <robbiealixsantos@gmail.com>
2016-11-29 13:25:15 -0800

    add initial tests for drawings feature

---
+++ spec/features/drawings_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "Drawings", type: :feature do
+  context 'a user' do
+    describe 'initial page load' do
+      include RSpecHtmlMatchers
+
+      it 'gives a selection for backgrounds' do
+        visit draw_path
+        expect(page).to have_content("pick a backdrop to draw on!")
+      end
+
+      it 'loads with images to select from' do
+        visit draw_path
+        expect('<img src="images/landscape.png" class="drawing-bg" id="land-bg">').to have_tag('img')
+      end
+
+    end
+  end
+end
